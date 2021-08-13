a5a81552dc2224a3aefa79f8080329a6d6e0cba8
jaymody <jaykmody@gmail.com>
2021-08-13 17:38:39 -0400

    minimal website

--- src/index.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./App";
-import "particles.js/particles";
-const particlesJS = window.particlesJS;
-
-ReactDOM.render(<App />, document.getElementById("root"));
-particlesJS("root", require("./particles.json"));
